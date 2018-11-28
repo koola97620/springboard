0e7b97619d81430502cee57ff7927e0712615def
Jaeyong <koola97620@nate.com>
2018-11-28 14:35:59 +0900

    이미지 업로드 시작.

--- src/main/java/com/board/dto/Image.java
+++ src/main/java/com/board/dto/Image.java
@@ -1,7 +1,18 @@
 package com.board.dto;
 
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Data
 public class Image {
-    private String savedName;
+    private Long id;
     private String originalName;
-    
+    private String saveName;
+    private String path;
+    private String type;
+    private Long size;
+    private Long boardId;
+    private Date regDate;
 }
