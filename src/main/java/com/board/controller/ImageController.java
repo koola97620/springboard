97bcbff7896e922cd586a30c86771551a362172e
Jaeyong <koola97620@nate.com>
2018-11-30 00:42:45 +0900

    이미지 업로드 시작.

--- src/main/java/com/board/controller/ImageController.java
+++ src/main/java/com/board/controller/ImageController.java
@@ -1,9 +1,16 @@
 package com.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ImageController {
+
+    @GetMapping("/uploadForm")
+    public String uploadForm() {
+
+        return "uploadForm";
+    }
 
 
 }
